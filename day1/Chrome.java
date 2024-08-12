7cbc4ce90035b090648b6a4610eb89a615b0307e
Nishi-debug-prog <nishigandhawagh7@gmail.com>
2024-08-12 22:41:35 +0530

    Add files via upload
    day1 assignments

---
+++ day1/Chrome.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		float version= 91.0f;
+		String developer="Google";
+				boolean isBeta=false;
+				int releaseYear=2008;
+						char shortcutKey='C';
+		System.out.println("Version of Chrome="+ version + 
+				"\n"+ "Developer="+ developer + 
+				"\n" + "Beta=" + isBeta + 
+				"\n" + "Year of release="+releaseYear +
+		"\n" + "Shortcut Key=" + shortcutKey);
+		
+		
+	}
+
+}
